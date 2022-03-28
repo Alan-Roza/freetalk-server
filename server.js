263467ca16304d34e6812c1bdb188a9912119982
alanroza <alancruz@outlook.com>
2022-03-28 00:07:20 -0300

    [wip]

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const User = require('./source/models/user')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const authRoutes = require('./source/routes/authRoutes')
+const { MongoClient } = require('mongodb')
+
 var cors = require('cors');
 
 const mongoDB = 'mongodb://localhost:27017/freetalk-db'
@@ -33,28 +35,72 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).t
   console.log(err)
 })
 
+var client = new MongoClient(mongoDB)
+
+var collection
+
 io.on('connection', (socket) => {
   console.log('a user connected')
+
+  socket.on('join', async (chatId) => {
+    try {
+      const response = await collection.findOne({ "_id": new mongoose.mongo.ObjectId(chatId) })
+      console.log(new mongoose.mongo.ObjectId(chatId), 'response')
+      if (!response) {
+        await collection.insertOne({ "_id": new mongoose.mongo.ObjectId(chatId), messages: [] })
+      }
+      socket.join(chatId)
+      socket.emit("joined", response)
+      socket.activeRoom = chatId
+    } catch (err) {
+      console.error(err)
+    }
+  })
+
+  socket.on('create', async (newChat) => {
+    const newId = new mongoose.mongo.ObjectId()
+    try {
+      await collection.insertOne({
+        "_id": new mongoose.mongo.ObjectId(newId),
+
+        references: newChat.references,
+        messages: [],
+        // receiverName: newChat.receiverName,
+        privateChat: newChat.privateChat
+
+      })
+      socket.join(newId)
+      socket.emit("joined", newId)
+      socket.activeRoom = newId
+
+      const response = await collection.findOne({ "_id": new mongoose.mongo.ObjectId(newId) })
+      socket.emit("chat-created", response)
+    } catch (err) {
+      console.error(err)
+    }
+  })
+
+  socket.on("message", async (chat) => {
+    collection.updateOne({ "_id": new mongoose.mongo.ObjectId(socket.activeRoom) }, {
+      "$push": {
+        "messages": {
+          message: chat.message,
+          senderId: new mongoose.mongo.ObjectId(chat.senderId),
+          createdAt: new Date(chat.createdAt)
+        }
+      }
+    })
+    const response = await collection.findOne({ "_id": new mongoose.mongo.ObjectId(socket.activeRoom) })
+    io.to(socket.activeRoom).emit("message", response)
+
+    Chats.find().then(result => {
+      io.emit('chat-updated', result)
+    })
+  })
 
   Chats.find().then(result => {
     socket.emit('output-messages', result)
   })
-
-  socket.on('new-chat', (chat) => {
-    const newChat = new Chats(chat)
-    newChat.save().then(() => {
-      console.log(chat, 'chat')
-      io.emit('chat-created', chat)
-    })
-  })
-
-  // socket.on('chat-message', (msg) => {
-  //   const message = new Chats({ msg })
-  //   message.save().then(() => {
-  //     console.log(msg, 'msg')
-  //     io.emit('message', msg)
-  //   })
-  // })
 
   socket.on('disconnect', () => {
     console.log('user disconnected')
@@ -62,5 +108,11 @@ io.on('connection', (socket) => {
 })
 
 httpServer.listen(PORT, async () => {
-  console.log(`listening on *:${PORT}`)
+  try {
+    await client.connect()
+    collection = client.db('freetalk-db').collection('chats')
+    console.log(`listening on *:${PORT}`)
+  } catch (err) {
+    console.error(err)
+  }
 })
